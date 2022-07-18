77ab75610e50b09a97f65ccded6975ea315efa35
lookininward <vinoth.michaelxavier@gmail.com>
2022-07-18 19:48:35 -0400

    Initial Commit

---
+++ TimeConversion/TimeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConversionApp.swift
+//  TimeConversion
+//
+//  Created by Michael Xavier on 2022-07-18.
+//
+
+import SwiftUI
+
+@main
+struct TimeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
